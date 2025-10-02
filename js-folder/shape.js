8322bdd67afd5ded372b6e3422577c95e009eb4a
jahangiralom86ffff <jahangiralom144155160@gmail.com>
2025-10-02 11:55:32 +0600

    New file add

--- js-folder/shape.js
+++ js-folder/shape.js
@@ -39,11 +39,59 @@ window.addEventListener('load', () => {
    if (preloder) {
       setTimeout(() => {
         preloder.style.display = "none";
-      }, 2000);
+      }, 100);
    }
 });
 
 //preloder start End//
+
+
+
+//wow js//
+const preducst = document.getElementById("preducst");
+
+preducst.addEventListener("mouseover", () => {
+  preducst.style.transform = "translateY(-20px)";
+  preducst.style.transition = "all 0.3s ease";
+});
+
+preducst.addEventListener("mouseout", () => {
+  preducst.style.transform = "translateY(0)";
+});
+
+
+
+
+let preducsts = document .getElementById("preducsts");
+
+preducsts.addEventListener("mouseover", () =>{
+  preducsts .style .transform = "translateY(-20px)";
+  preducsts .style .transition = "all 0.3s ease";
+
+});
+
+preducsts .addEventListener("mouseout", () =>{
+  preducsts .style .transform = "translateY(0)";
+
+});
+
+
+
+let produts = document .getElementById("produts");
+
+produts.addEventListener("mouseover", () =>{
+  produts .style .transform = "translateY(-20px)";
+  produts .style .transition = "all 0.3s ease";
+
+});
+
+produts.addEventListener("mouseout", () =>{
+    produts .style .transform = "translateY(0)";
+
+});
+
+
+
 
 
 
